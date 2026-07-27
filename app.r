3d83400ad2df84cb65eeb4a3206c492f8703e30c
hannahhodgemurrer <hehodge05@gmail.com>
2026-07-27 19:12:01 -0400

    size

--- app.r
+++ app.r
@@ -332,8 +332,8 @@ server <- function(input, output, session) {
                       )
                     )
                   ),
-                  column(4, textInput("add_item", "Item Name*", placeholder = "e.g., Sweater, Shoes")),
-                  column(3, textInput("add_size", "Size / Options", placeholder = "e.g., Medium, Size 8")),
+                  column(5, textInput("add_item", "Item Name*", placeholder = "e.g., Sweater, Shoes")),
+                  column(4, textInput("add_size", "Size / Options", placeholder = "e.g., Medium, Size 8")),
                   column(12, textInput("add_link", "URL / Product Link", placeholder = "e.g., https://..."))
               
                 ),
@@ -404,7 +404,7 @@ server <- function(input, output, session) {
                       )
                     )
                   ),
-                  column(4, textInput("add_other_item", "Item Name*", placeholder = "e.g., Sweater, Shoes")),
+                  column(5, textInput("add_other_item", "Item Name*", placeholder = "e.g., Sweater, Shoes")),
                   column(3, textInput("add_other_size", "Size / Options", placeholder = "e.g., Medium, Size 8")),
                   column(12, textInput("add_other_link", "URL / Product Link", placeholder = "e.g., https://..."))
                 ),
